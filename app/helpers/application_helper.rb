02b464a3f2be7528f60d759063267521bf9f1808
Kaciano Ghelere <themohawkeagle@gmail.com>
2015-07-23 17:21:42 -0300

    Mais alterações no layout, não completado

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,24 @@ module ApplicationHelper
 	def link_icon(icon, text)
 		"<span class='glyphicon glyphicon-#{icon}'></span> #{text}".html_safe
 	end
+
+	def btn_submit
+		"btn btn-primary btn-md"
+	end
+
+	def btn_create
+		"btn btn-primary btn-sm"
+	end
+
+	def btn_show
+		"btn btn-default btn-sm"
+	end
+	
+	def btn_edit
+		"btn btn-info btn-sm"
+	end
+
+	def btn_destroy
+		"btn btn-danger btn-sm"
+	end
 end
